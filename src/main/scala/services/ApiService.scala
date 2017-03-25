a3ef54d8d917a85e3cbce6960c7eee004e469114
Antonio Pacheco <greven77@gmail.com>
2017-03-25 23:16:41 +0000

    Exposed the missing controllers to web server

--- src/main/scala/services/ApiService.scala
+++ src/main/scala/services/ApiService.scala
@@ -1,21 +1,30 @@
 package services
 
-import repository.{BookRepository, CategoryRepository}
+import repository.{AuthRepository, BookRepository, CategoryRepository, UserRepository}
 import akka.http.scaladsl.server.Directives._
-import controllers.{BookController, CategoryController}
+import controllers.{AuthController, BookController, CategoryController, UserController}
 
 import scala.concurrent.ExecutionContext
 
-class ApiService(categoryRepository: CategoryRepository, bookRepository: BookRepository,
- tokenService: TokenService)(implicit executor: ExecutionContext) {
+class ApiService(
+  categoryRepository: CategoryRepository,
+  bookRepository: BookRepository,
+  authRepository: AuthRepository,
+  userRepository: UserRepository,
+  tokenService: TokenService)(implicit executor: ExecutionContext) {
 
   val categoryController = new CategoryController(categoryRepository)
 
   val bookController = new BookController(bookRepository, tokenService)
+  val authController = new AuthController(authRepository, tokenService)
+  val userController = new UserController(userRepository)
+
 
   def routes =
     pathPrefix("api") {
       categoryController.routes ~
-      bookController.routes
+      bookController.routes ~
+      authController.routes ~
+      userController.routes
     }
 }
